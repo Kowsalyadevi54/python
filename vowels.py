c4c792fbed4348cd597210850b7213102d4e024c
Kowsalyadevi54 <49307376+Kowsalyadevi54@users.noreply.github.com>
2019-04-11 12:46:52 +0530

    vowels.py

---
+++ vowels.py
@@ -0,0 +1,5 @@
+ch=input("please Enter your character:")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+ print("The character",ch,"is a vowel")
+else:
+  print("The character",ch,"is a consonant")
