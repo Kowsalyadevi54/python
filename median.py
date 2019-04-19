81a935f10bc967811e7dbe93a57c6ab18c5a7de7
Kowsalyadevi54 <49307376+Kowsalyadevi54@users.noreply.github.com>
2019-04-19 09:07:33 -0700

    median.py

--- median.py
+++ median.py
@@ -1,8 +1,8 @@
 def median(lst):
    n=len(lst)
    if n<1:
-     return none
+      return none
    if n%2==1:
-     return sorted(lst)[n//2]
+      return sorted(lst)[n//2]
    else:
-     return sum(sorted(lst)[n//2-1:n//2+1])/2.0
+      return sum(sorted(lst)[n//2-1:n//2+1])/2.0
