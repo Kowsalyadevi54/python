ef480f0fa8079f85a20aea7c5de26412fd150da6
Kowsalyadevi54 <49307376+Kowsalyadevi54@users.noreply.github.com>
2019-04-18 20:36:00 -0700

    sort.py

---
+++ sort.py
@@ -0,0 +1,9 @@
+def sortit(arr,n):
+   for i in range(n):
+       arr[i]=i+1
+   if__name__=='__main__':
+       arr=[10,8,7,9,2,4,6,1,3,5]
+       n=len(arr)
+       sortit(arr,n)
+   for i in range(n):
+       print(arr[i],end=" ")
