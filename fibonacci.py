ff64522a0a6bcdcb4d44513fa78117b975d2ef8e
Kowsalyadevi54 <49307376+Kowsalyadevi54@users.noreply.github.com>
2019-04-23 16:00:27 +0530

    fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+n=input("Enter number:")
+n=int(n)
+a=1
+b=1
+if(n==1):
+    print(a,end='')
+count=0
+if(n>1):
+   while(count<n):
+      if(count==n-1):
+         print(a,end='')
+      else:
+         print(n1,end='')
+      nth=n1+n2
+      n1=n2
+      n2=nth
+      count=count+1
