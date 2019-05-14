c3d135669d32c3cdb0d4e9aff2bba8d5ee1993e6
Kowsalyadevi54 <49307376+Kowsalyadevi54@users.noreply.github.com>
2019-05-14 10:49:57 +0530

    88lc.py

---
+++ 88lc.py
@@ -0,0 +1,11 @@
+x1,y=map(int,raw_input().split())
+if x1>y:
+    greater=x1
+else:
+    greater=y
+while(True):
+    if((greater % x1==0) and (greater % y==0)):
+        lcm=greater
+        break
+    greater=greater+1
+print(lcm)
